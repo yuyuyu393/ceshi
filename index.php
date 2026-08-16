8335e3bdc3ed37e30bcb10bb92ed4f662173a490
yuyuyu393 <2831570002@qq.com>
2026-08-16 11:54:22 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-:8080 {
-    reverse_proxy https://www.huzhan.com
+<?php
+require 'vendor/autoload.php'; // 引入Composer的自动加载文件
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+$url = 'https://www.huzhan.com/api/data'; // 目标URL
+$method = 'GET'; // 请求方法
+$headers = [
+    'Content-Type' => 'application/json',
+    'Authorization' => 'Bearer YOUR_ACCESS_TOKEN' // 如果有需要认证的头部
+];
+$options = [
+    'headers' => $headers, // 设置头部信息
+];
+
+try {
+    $client = new Client();
+    $response = $client->request($method, $url, $options);
+    echo $response->getBody(); // 输出响应内容
+} catch (RequestException $e) {
+    echo $e->getMessage(); // 捕获并处理异常信息
 }
+?>
